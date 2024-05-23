d3a2d3cb10a4fda82c6b134f303ffc9cb6ee2e6b
Othielgh <57671465+Othielgh@users.noreply.github.com>
2024-05-23 20:33:06 +0200

    update

--- Thunar/accels.scm
+++ Thunar/accels.scm
@@ -83,6 +83,7 @@
 ; (gtk_accel_path "<Actions>/ThunarStandardView/toggle-sort-order" "")
 ; (gtk_accel_path "<Actions>/ThunarActionManager/open-in-new-window" "<Primary><Shift>o")
 ; (gtk_accel_path "<Actions>/ThunarWindow/view-configure-toolbar" "")
+; (gtk_accel_path "<Actions>/ThunarBookmarks/678875a6b3881810b8e619344d52fcd3" "")
 ; (gtk_accel_path "<Actions>/ThunarWindow/view-location-selector-menu" "")
 ; (gtk_accel_path "<Actions>/ThunarWindow/view-statusbar" "")
 ; (gtk_accel_path "<Actions>/ThunarWindow/close-all-windows" "<Primary><Shift>w")
