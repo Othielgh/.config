31edc0d534be2688c468c926a6397b67332f37e0
AJ <57671465+Othielgh@users.noreply.github.com>
2025-05-19 23:55:26 +0200

    Updating configs, fixed systemd wallpaper timer

--- hypr/scripts/randomPaper.sh
+++ hypr/scripts/randomPaper.sh
@@ -1,18 +1,18 @@
-#!/bin/sh
+#!/bin/bash
+
+hyprctl hyprpaper unload all
 
 case $(date +%H) in
-22 | 23 | 00 | 01 | 02 | 03 | 04 | 05 | 06 | 07) # First 8 hours of the day
-  # Uncomment below to setup the image you wish to display as your
-  # wallpaper if you run this script during the first 8 hours of the
-  # day
-  swaybg -i ~/.config/Wallpapers/Wall.jpeg
+22 | 23 | 00 | 01 | 02 | 03 | 04 | 05 | 06 | 07)
+  hyprctl hyprpaper preload ~/.config/Wallpapers/asciiEarth.jpeg
+  hyprctl hyprpaper wallpaper ",~/.config/Wallpapers/asciiEarth.jpeg"
   ;;
-08 | 09 | 10 | 11 | 12 | 13 | 14 | 15) # Middle 8 hours of the day
-  # Same as above, but for the middle 8 hours of the day
-  swaybg -i ~/.config/Wallpapers/summer-day.png
+08 | 09 | 10 | 11 | 12 | 13 | 14 | 15)
+  hyprctl hyprpaper preload ~/.config/Wallpapers/summer-day.png
+  hyprctl hyprpaper wallpaper ",~/.config/Wallpapers/summer-day.png"
   ;;
 16 | 17 | 18 | 19 | 20 | 21)
-  # Same as above, but for the final 8 hours of the day
-  swaybg -i ~/.config/Wallpapers/evening.png
+  hyprctl hyprpaper preload ~/.config/Wallpapers/evening.png
+  hyprctl hyprpaper wallpaper ",~/.config/Wallpapers/evening.png"
   ;;
 esac
